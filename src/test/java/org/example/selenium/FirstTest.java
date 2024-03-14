36e09e0926d814cb01d5ee5a3fc62c63c7e20eaa
Евгений <Keshavoloshin@mail.ru>
2024-03-14 13:04:11 +0300

    Create test

---
+++ src/test/java/org/example/selenium/FirstTest.java
@@ -0,0 +1,27 @@
+package org.example.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void openBroutherTest() {
+        driver.get("https://www.google.ru/");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
+}
